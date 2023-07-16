4700b2796193b863602043a802fd220256c24b5f
kevin1234h8 <kevinang445566@gmail.com>
2023-07-16 15:54:31 +0700

    deploy loading

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -6,8 +6,8 @@ const LoadingSpinner = () => {
     //   <div className="spinner"></div>
     // </div>
     <div
-      className="fixed inset-0 z-50 flex items-center justify-center"
-      style={{ background: "rgba(0,0,0,0.3)" }}
+      className="fixed inset-0 z-50 flex items-center justify-center bg-white dark:bg-[#0e1011]"
+      // style={{ background: "rgba(0,0,0,0.3" }}
     >
       <img src={loading} alt="" className="w-[40px]" />
     </div>
